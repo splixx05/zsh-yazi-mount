6e70408265d3bbd46e16572e2cced7a3260f9ecb
ricky <splixx05@gmail.com>
2025-10-18 08:38:41 +0200

    update function

--- zsh-yazi-mount.zsh
+++ zsh-yazi-mount.zsh
@@ -1,23 +1,19 @@
 # open a new tmux window with yazi for the phone
 
-function :phone ()
-{
- if [[ -n $TMUX ]]; then
-   tmux new-window -n phone && "$ZSH/custom/plugins/zsh-yazi-mount/src/mtp-mnt.zsh"
- else
-$ZSH/custom/plugins/zsh-yazi-mount/src/mtp-mnt.zsh
- fi
+function :phone() {
+  if [[ -n $TMUX ]]; then
+    tmux new-window -n phone && "$HOME/.zsh/plugins/local/zsh-yazi-mount/src/mtp-mnt.zsh"
+  else
+    "$HOME/.zsh/plugins/local/zsh-yazi-mount/src/mtp-mnt.zsh"
+  fi
 }
-
 
 # open a new tmux window with yazi for the usb
 
-function :usb ()
-{
- if [[ -n $TMUX ]]; then
-   tmux new-window -n usb "$ZSH/custom/plugins/zsh-yazi-mount/src/usb-mnt.zsh"
- else
-$ZSH/custom/plugins/zsh-yazi-mount/src/usb-mnt.zsh
- fi
+function :usb() {
+  if [[ -n $TMUX ]]; then
+    tmux new-window -n usb "$HOME/.zsh/plugins/local/zsh-yazi-mount/src/mtp-mnt.zsh"
+  else
+    "$HOME/.zsh/plugins/local/zsh-yazi-mount/src/mtp-mnt.zsh"
+  fi
 }
-
